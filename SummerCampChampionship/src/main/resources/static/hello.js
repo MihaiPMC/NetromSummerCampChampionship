7f8de5f86808f0227524dd88d5926e7d67fce67c
Mihai Patru <mihaipatru05@gmail.com>
2023-07-11 04:14:27 +0300

    added the table and header row

--- SummerCampChampionship/src/main/resources/static/hello.js
+++ SummerCampChampionship/src/main/resources/static/hello.js
@@ -1,28 +1,34 @@
 function makeTable(container, data) {
     var table = $("<table/>").addClass('playerTable');
-    $.each(data, function(rowIndex, r) {
+
+    var headerRow = $("<tr/>");
+    headerRow.append($("<th/>").text("ID"));
+    headerRow.append($("<th/>").text("FirstName"));
+    headerRow.append($("<th/>").text("LastName"));
+    headerRow.append($("<th/>").text("Age"));
+    table.append(headerRow);
+    $.each(data, function (rowIndex, r) {
+
         var row = $("<tr/>");
-        $.each(r, function(colIndex, c) {
-            row.append($("<t"+(rowIndex == 0 ?  "h" : "d")+"/>").text(c));
+        $.each(r, function (colIndex, c) {
+            row.append($("<td/>").text(c));
         });
         table.append(row);
     });
     return container.append(table);
 }
 
-$(document).ready(function() {
-
-    $.ajax(
-        {
-            url:" http://localhost:8080/player/all",
-            type:"GET",
-            dataType:"json",
-            success: function(data) {
-                var cityTable = makeTable($(document.body), data);
-            },
-            error: function(data) {
-                alert('Error: '+data);
-            }
+$(document).ready(function () {
+    $.ajax({
+        url: "http://localhost:8080/player/all",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            var tableContainer = $("#tableContainer");
+            makeTable(tableContainer, data);
+        },
+        error: function (data) {
+            alert('Error: ' + data);
         }
-    )
+    });
 });
