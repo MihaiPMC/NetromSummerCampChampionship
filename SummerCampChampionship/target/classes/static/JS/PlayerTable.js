1ab3cf346a9239e69e150e24b3cf0b7d6770cb91
Mihai Patru <mihaipatru05@gmail.com>
2023-07-12 04:39:14 +0300

    created a button to add a new player

--- SummerCampChampionship/target/classes/static/JS/PlayerTable.js
+++ SummerCampChampionship/target/classes/static/JS/PlayerTable.js
@@ -18,6 +18,11 @@ function makeTable(container, data) {
     return container.append(table);
 }
 
+function showForm() {
+    var form = document.getElementById("playerForm");
+    form.style.display = "block";
+}
+
 $(document).ready(function () {
     $.ajax({
         url: "http://localhost:8080/player/all",
